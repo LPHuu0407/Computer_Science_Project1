964468447ca7b10da361f63a37e782f3314bd9a5
Le Phuoc Huu <lphuu04072003@gmail.com>
2023-10-12 09:37:35 +0700

    Update code

--- PRACTICE/DEMO_GENETIC_ALGORITHM/Genetic_Algorithm.py
+++ PRACTICE/DEMO_GENETIC_ALGORITHM/Genetic_Algorithm.py
@@ -20,21 +20,20 @@ class Individual(object):
     def __init__(self, Value_Chromosome):
         self.chromosome = Value_Chromosome
         self.fitness = self.Fitness()
-    @classmethod
+    
     
     # Hàm 'Genetic_Mutation' được dùng tạo ra các gen đột biến
     def Genetic_Mutation(self):
         global Genes
         Gen = random.choice(Genes)
         return Gen
-    @classmethod
+    
     
     # Tạo ra nhiễm sắc thể hoặc một chuỗi
     def Create_Chromosome(self):
         global Target
         Chromosome_Len = len(Target)
         return [self.Genetic_Mutation() for _ in range(Chromosome_Len)]
-    @classmethod
     
     # Thực hiện giao phối, sinh ra con cái từ 2 nhiễm sắc thể
     def Mating(self, Parents):
@@ -42,19 +41,23 @@ class Individual(object):
         for Parent_1, Parent_2 in zip(self.chromosome, Parents.chromosome):
             # Tạo xác suất ngẫu nhiên
             Probability = random.random()
-            if Probability < 0.45: # Nếu xác suất nhỏ hơn 0.45
+            if Probability < 0.45: # Nếu xác suất nhỏ hơn 0.45 thì chèn gen của Parent_1
                 Child_Chromosome.append(Parent_1)
-            elif Probability < 0.90: # Nếu xác suất nhỏ hơn 0.90
+            elif Probability < 0.90: # Nếu xác suất nằm trong khoảng từ 0.45 - 0.90 thì chèn gen của Parent_2
                 Child_Chromosome.append(Parent_2)
             else: # Trường hợp còn lại
                 Child_Chromosome.append(self.Genetic_Mutation())
         
-        return Individual(Child_Chromosome)
-    # def Fitness(self):
-    #     global Target
-    #     fitness = 0
-    #     for 
-
+        return Individual(Child_Chromosome) # Tạo ra cá thể mới
+    
+    # Đánh giá độ thích nghi của nhiễm sắc thể, so với chuỗi mục tiêu
+    def Fitness(self):
+        global Target
+        fitness = 0
+        for gs, gt in zip(self.chromosome, Target):
+            if gs != gt:
+                fitness += 1
+        return fitness
 
 
 
